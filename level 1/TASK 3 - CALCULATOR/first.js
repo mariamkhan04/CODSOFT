0bf10876138f3b364718025430f060cd6ca4b86f
mariamkhan04 <125134327+mariamkhan04@users.noreply.github.com>
2024-06-27 20:54:25 +0500

    default input set

--- level 1/TASK 3 - CALCULATOR/first.js
+++ level 1/TASK 3 - CALCULATOR/first.js
@@ -1,6 +1,6 @@
 let screen = document.getElementById('screen');
 buttons = document.querySelectorAll('button');
-let screenValue  = '';
+let screenValue  = '0';
 for (item of buttons) {
     item.addEventListener('click', (e)=>{
         buttonText = e.target.innerText;
@@ -18,13 +18,17 @@ for (item of buttons) {
             screenValue += buttonText;
             screen.value = screenValue;
         } else if(buttonText == 'AC'){
-            screenValue = '';
+            screenValue = '0';
             screen.value = screenValue;
         } else if(buttonText == '='){
             screenValue = eval(screen.value);
             screen.value = screenValue;
-        } else{
-            screenValue += buttonText;
+        } else {
+            if (screenValue === '0') {
+                screenValue = buttonText;
+            } else {
+                screenValue += buttonText;
+            }
             screen.value = screenValue;
         }
     })
